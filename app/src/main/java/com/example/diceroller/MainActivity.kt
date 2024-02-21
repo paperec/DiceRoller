3055442330a693fa26bb4a09b511feb4dc1f60ab
paperec <91558365+paperec@users.noreply.github.com>
2024-02-21 10:49:59 +0100

    Apartado 5

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -6,12 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.diceroller.ui.theme.DiceRollerTheme
 
@@ -32,7 +34,11 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
         modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ){
-
+        Button(onClick = { /*TODO*/ }) {
+            Text(
+                stringResource(R.string.roll)
+            )
+        }
     }
 }
 
